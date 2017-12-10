0f8f169fc1c74b3213da720229e1d2f3e536f04f
Mark Shelton <mark@shelton.id.au>
2017-12-10 13:54:57 +0800

    added marcdb scripts for MARCXML -> SQL conversion

---
+++ src/scripts/marcdb/db.py
@@ -0,0 +1,21 @@
+from elixir import metadata, session, setup_all, create_all, drop_all 
+	
+"""
+a very slim wrapper around some elixir functions
+to connect to, create, commit to and clean a marcdb
+"""
+	
+def connect(uri):
+	metadata.bind = uri
+	setup_all()
+	
+def create():
+	setup_all()
+	create_all()
+	
+def commit():
+	session.flush()
+	session.clear()
+	
+def zap():
+	drop_all()	

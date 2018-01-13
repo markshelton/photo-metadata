3541e72ab4a0fdfddb0b4de3def81a735b3c0758
Mark Shelton <mark@shelton.id.au>
2018-01-13 18:14:24 +0800

    minor changes to types file

--- src/lib/_types.py
+++ src/lib/_types.py
@@ -1,55 +1,45 @@
 ##########################################################
 # Built-in Types
 
-from typing import (
-    List, Optional, NewType, Dict, Iterator,
-    IO, Union, Callable, Any, Pattern, Tuple
-)
+from typing import *
+from typing import Pattern
 from mypy_extensions import TypedDict, KwArg
 
 ##########################################################
 # External Types
 
-import datetime
-import pymarc
-import sqlalchemy.engine
-import sqlalchemy.orm
-import sqlalchemy.ext
-import numbers
-import tensorflow as tf
-import numpy as np
+from pymarc.record import Record as PymarcRecord
+from pymarc.field import Field as PymarcField
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import Session
+from datetime import date as Date
+from pathlib import Path as FilePath
+from numbers import Real
 
-Date = datetime.date
-Record = pymarc.Record
-Field = pymarc.Field
-Engine = sqlalchemy.engine.Engine
-Session = sqlalchemy.orm.Session
-Tensor = tf.Tensor
-Array = np.array
-Real = numbers.Real
 Schema = Any #sqlalchemy - base type
 Image = Any #OpenCV
 Rectangle = Any #dlib
 BoundingBox = Any #dlib
 Shape = Any #dlib
 TFSession = Any #tf
+Face = Any
+File = Any
 
 ##########################################################
 # Local Types
 
-FilePath = str
-DirPath = str
-File = IO[str]
+DirPath = FilePath
+Label = int
+
 Match = Dict[str, str]
+Features = List[Real]
 ParsedRecord = Dict[str, Any]
 JSONType = Union[List[ParsedRecord], ParsedRecord]
+
 Tag = TypedDict('Tag', {"field": str, "subfield": str})
-Dates = TypedDict("Dates", {"start": Optional[Date], "end": Optional[Date]})
 Size = TypedDict("Size", {"width": int, "height": int})
-Location = TypedDict("Location", {
-    "latitude": float, "longitude": float, "bb_size": float, "query": str, "address": str
-    }, total=False
-)
+Location = TypedDict("Location", {"latitude": float, "longitude": float, "bb_size": float, "query": str, "address": str}, total=False)
+Dates = TypedDict("Dates", {"start": Optional[Date], "end": Optional[Date]})
 Address = TypedDict("Address", {
     'street_number': Optional[str], 'street_name': Optional[str], 'street_type': Optional[str],
     'suburb_name': str, 'state': str, 'country': str, 'keywords': List[Optional[str]]
@@ -60,11 +50,8 @@ DBConfig = TypedDict("DBConfig", {
     'username': Optional[str], 'password': Optional[str],
     }, total=False
 )
-Face = Any
-Features = List[Real]
-Label = int
-DatasetRecord = TypedDict("DatasetRecord", {
-    "features": Features, "label": Label, "path": FilePath
-    }, total=False
-)
+
+DatasetRecord = TypedDict("DatasetRecord", {"features": Features, "label": Label, "path": FilePath}, total=False)
 Dataset = List[DatasetRecord]
+
+    

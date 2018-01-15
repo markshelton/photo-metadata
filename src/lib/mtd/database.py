76084c1a80ac7fb48c4945c1d2b949ad287bbf26
Mark Shelton <mark@shelton.id.au>
2018-01-15 14:33:37 +0800

    added export to hdf5 function

--- src/lib/mtd/database.py
+++ src/lib/mtd/database.py
@@ -6,15 +6,18 @@
 # Standard Library Imports
 
 import csv
+import h5py
 import json
 import logging
+import numpy as np
 import os
+import time
 from contextlib import contextmanager
 
 ##########################################################
 # Third Party Imports
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.engine import url
 from sqlalchemy.exc import IntegrityError, DataError
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -23,7 +26,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 # Local Imports
 
 from thickshake.mtd.schema import Base
-from thickshake.utils import maybe_make_directory, open_file, json_serial
+from thickshake.utils import maybe_make_directory, open_file, json_serial, log_progress
 from thickshake._types import *
 
 ##########################################################
@@ -68,6 +71,21 @@ def manage_db_session(db_engine: Engine) -> Iterator[Session]:
         session.close()
 
 
+def dump_database(db_config: DBConfig) -> List[Dict[str, Any]]:
+    db_engine = initialise_db(db_config)
+    with manage_db_session(db_engine) as session:
+        sql_text =  "SELECT *\n"
+        sql_text += "FROM image\n"
+        sql_text += "NATURAL LEFT JOIN collection\n"
+        sql_text += "NATURAL LEFT JOIN collection_subject\n"
+        sql_text += "NATURAL LEFT JOIN collection_location\n"
+        sql_text += "NATURAL LEFT JOIN collection_topic\n"
+        sql_text += "NATURAL LEFT JOIN subject;"
+        result = session.execute(text(sql_text)).fetchall()
+        result = [dict(record) for record in result]
+        return result  #Total = 52,672 -- Check?
+
+
 def export_records_to_csv(records: List[ParsedRecord], output_file: FilePath) -> None:
     if not records: return None
     with open_file(output_file, 'w+', encoding='utf-8') as outfile:
@@ -89,6 +107,21 @@ def export_records_to_log(records: List[Any]) -> None:
     if not records: return None
     for record in records:
         logger.info(str(record))
+
+
+def export_records_to_hdf5(db_config: DBConfig, metadata_file: FilePath) -> None:
+    result = dump_database(db_config)
+    with h5py.File(metadata_file, "a") as f:
+        total = len(result)
+        start_time = time.time()
+        for i, record in enumerate(result):
+            key = record["image_id"]
+            grp = f.require_group(key)
+            index = str(len(grp.keys()) + 1)
+            record_serial = json.dumps(record, default=json_serial)
+            dt = h5py.special_dtype(vlen=str)
+            grp.require_dataset(index, data=record_serial, shape=(1,), dtype=dt)
+            log_progress(i+1, total, start_time, interval=100)
 
 
 ##########################################################

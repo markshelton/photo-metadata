d1c9379faa741ae05e5619bb63b94a1193cab047
Mark Shelton <mark@shelton.id.au>
2018-01-20 17:59:02 +0800

    created Constructor mixin to manage reader-to-schema connection

--- src/lib/mtd/schema.py
+++ src/lib/mtd/schema.py
@@ -24,7 +24,14 @@ Base = declarative_base() # type: Any
 ##########################################################
 
 
-class Collection(Base):
+class Constructor(object):
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            if hasattr(self, k):
+                setattr(self, k, v)
+
+
+class Collection(Constructor, Base):
     __tablename__ = "collection"
 
     collection_id = Column(String(30), primary_key=True)
@@ -33,7 +40,9 @@ class Collection(Base):
     note_summary = Column(String(255))
     series_title = Column(String(255))
     series_volume = Column(String(30))
-    date_created = Column(Date)
+    date_created = Column(String(255))
+    date_created_approx = Column(String(255))
+    date_created_parsed = Column(Date)
     physical_extent = Column(String(255))
     physical_details = Column(String(255))
 
@@ -45,13 +54,15 @@ class Collection(Base):
     def __repr__(self):
         return "<Collection(id='%s', title='%s')>" % \
             (self.collection_id, self.note_title)
+    
 
 
-class Subject(Base):
+class Subject(Constructor, Base):
     __tablename__ = "subject"
 
     subject_name = Column(String(255), primary_key=True)
     subject_type = Column(String(30))  # Building | Person
+    subject_dates = Column(String(255))
     subject_start_date = Column(Date)
     subject_end_date = Column(Date)
 
@@ -65,11 +76,11 @@ class Subject(Base):
              self.subject_end_date)
 
 
-class Image(Base):
+class Image(Constructor, Base):
     __tablename__ = "image"
 
     image_id = Column(String(30), primary_key=True)
-    image_url_main = Column(String(255))
+    image_url = Column(String(255))
     image_url_raw = Column(String(255))
     image_url_thumb = Column(String(255))
     image_note = Column(String(255), nullable=False)
@@ -88,17 +99,11 @@ class Image(Base):
             (self.image_id, self.image_note, self.image_url, self.collection)
 
 
-class CollectionSubject(Base):
+class CollectionSubject(Constructor, Base):
     __tablename__ = "collection_subject"
 
-    collection_id = Column(
-        String(30),
-        ForeignKey("collection.collection_id"),
-        primary_key=True)
-    subject_name = Column(
-        String(255),
-        ForeignKey("subject.subject_name"),
-        primary_key=True)
+    collection_id = Column(String(30), ForeignKey("collection.collection_id"), primary_key=True)
+    subject_name = Column(String(255), ForeignKey("subject.subject_name"), primary_key=True)
     subject_is_main = Column(Boolean, primary_key=True)
     subject_relation = Column(String(255))
 
@@ -106,19 +111,16 @@ class CollectionSubject(Base):
     subject = relationship("Subject", back_populates="collections")
 
 
-class CollectionTopic(Base):
+class CollectionTopic(Constructor, Base):
     __tablename__ = "collection_topic"
 
-    collection_id = Column(
-        String(30),
-        ForeignKey("collection.collection_id"),
-        primary_key=True)
+    collection_id = Column(String(30), ForeignKey("collection.collection_id"), primary_key=True)
     topic = Column(String(255), primary_key=True)
 
     collection = relationship("Collection", back_populates="topics")
 
 
-class CollectionLocation(Base):
+class CollectionLocation(Constructor, Base):
     __tablename__ = "collection_location"
 
     collection_id = Column(

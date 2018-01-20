100337589ded9461631fa25695383498fb118163
Mark Shelton <mark@shelton.id.au>
2018-01-20 21:28:27 +0800

    added functions for loading and saving columns from the database to help with parsing

--- src/lib/mtd/database.py
+++ src/lib/mtd/database.py
@@ -7,21 +7,23 @@
 
 import logging
 from contextlib import contextmanager
-from envparse import env
 
 ##########################################################
 # Third Party Imports
 
-from sqlalchemy import create_engine, text
+import pandas as pd
+from envparse import env
+from sqlalchemy import create_engine, text, func
 from sqlalchemy.engine import url
 from sqlalchemy.exc import IntegrityError, DataError
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.inspection import inspect
+from sqlalchemy.orm import scoped_session, sessionmaker, load_only
 
 ##########################################################
 # Local Imports
 
 from thickshake.mtd.schema import Base
-from thickshake.utils import maybe_make_directory
+from thickshake.utils import maybe_make_directory, setup_logging, setup_warnings
 from thickshake.types import *
 
 ##########################################################
@@ -43,6 +45,17 @@ logger = logging.getLogger(__name__)
 # Functions
 
 
+def get_class_by_table_name(table_name: str) -> Any:
+  for c in Base._decl_class_registry.values():
+    if hasattr(c, '__tablename__') and c.__tablename__ == table_name:
+      return c
+
+def get_primary_key(table_name: str = None, model: Any = None) -> List[str]:
+    assert model is not None or table_name is not None
+    if model is None: model = get_class_by_table_name(table_name)
+    pk_columns = inspect(model).primary_key
+    return [pk.name for pk in pk_columns]
+
 def make_db_tables(db_engine: Engine) -> None:
     Base.metadata.create_all(db_engine)
 
@@ -51,14 +64,14 @@ def remove_db_tables(db_engine: Engine) -> None:
     Base.metadata.drop_all(db_engine)
 
 
-def make_db_engine(db_config: DBConfig) -> Engine:
+def make_db_engine(db_config: DBConfig=DB_CONFIG) -> Engine:
     db_url = url.URL(**db_config)
     maybe_make_directory(db_config["database"])
     db_engine = create_engine(db_url, encoding='utf8', convert_unicode=True)
     return db_engine
 
 
-def initialise_db(db_config: DBConfig, clear_flag: bool = False, **kwargs: Any) -> Engine:
+def initialise_db(db_config: DBConfig=DB_CONFIG, clear_flag: bool = False, **kwargs: Any) -> Engine:
     db_engine = make_db_engine(db_config)
     if clear_flag: remove_db_tables(db_engine)
     make_db_tables(db_engine)
@@ -81,8 +94,8 @@ def manage_db_session(db_engine: Engine) -> Iterator[Session]:
         session.close()
 
 
-def dump_database(db_config: DBConfig, **kwargs: Any) -> List[Dict[str, Any]]:
-    db_engine = initialise_db(db_config)
+def dump_database(db_config: DBConfig=DB_CONFIG, **kwargs: Any) -> List[Dict[str, Any]]:
+    db_engine = initialise_db(db_config, **kwargs)
     with manage_db_session(db_engine) as session:
         sql_text =  "SELECT *\n"
         sql_text += "FROM image\n"
@@ -96,4 +109,51 @@ def dump_database(db_config: DBConfig, **kwargs: Any) -> List[Dict[str, Any]]:
         return result
 
 
+def load_column(table: str, column: str, db_config: DBConfig=DB_CONFIG) -> Series:
+    db_engine = initialise_db(db_config)
+    with manage_db_session(db_engine) as session:
+        model = get_class_by_table_name(table)
+        result = session.query(model).all()
+        pk = get_primary_key(model=model)
+        records = [record.__dict__ for record in result]
+        df = pd.DataFrame(data=records)
+        df.set_index(keys=pk, inplace=True)
+        series = df[column]
+        return series
+
+#TODO
+def save_column(table: str, column: str, series: Series, db_config: DBConfig=DB_CONFIG) -> None:
+    db_engine = initialise_db(db_config)
+    with manage_db_session(db_engine) as session:
+        model = get_class_by_table_name(table)
+        pass #TODO
+
+
+def save_columns(output_map: Dict[str, Tuple[str, str]], data: DataFrame, db_config: DBConfig=DB_CONFIG) -> None:
+    for column in data:
+        if column in output_map:
+            save_column(*output_map[column], series=data[column], db_config=db_config)
+
+
+def inspect_database(db_config: DBConfig=DB_CONFIG) -> None:
+    db_engine = initialise_db(db_config)
+    with manage_db_session(db_engine) as session:
+        for table_name in db_engine.table_names():
+            model = get_class_by_table_name(table_name)
+            num_records = session.query(func.count('*')).select_from(model).scalar()
+            logger.info("Table: %s, Records: %s", table_name, num_records)
+
+
 ##########################################################
+# Main
+
+
+def main():
+    load_column()
+
+
+if __name__ == "__main__":
+    setup_logging()
+    setup_warnings()
+    main()
+

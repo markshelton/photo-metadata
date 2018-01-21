56fc84c735ff6bbaa4a5f98ca12029084d19440d
Mark Shelton <mark@shelton.id.au>
2018-01-21 22:33:07 +0800

    added Location and ImageLocation, updated CollectionLocation in DB schema

--- src/lib/mtd/schema.py
+++ src/lib/mtd/schema.py
@@ -8,7 +8,7 @@
 ##########################################################
 # Third Party Imports
 
-from sqlalchemy import Column, ForeignKey, String, Boolean, Date, Integer
+from sqlalchemy import Column, ForeignKey, String, Boolean, Date, Integer, Numeric
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -22,7 +22,7 @@ from thickshake.types import Any
 Base = declarative_base() # type: Any
 
 ##########################################################
-
+# Mix-ins
 
 class Constructor(object):
     def __init__(self, **kwargs):
@@ -30,6 +30,8 @@ class Constructor(object):
             if hasattr(self, k):
                 setattr(self, k, v)
 
+##########################################################
+# Data Tables
 
 class Collection(Constructor, Base):
     __tablename__ = "collection"
@@ -93,10 +95,41 @@ class Image(Constructor, Base):
     collection_id = Column(String(30), ForeignKey("collection.collection_id"))
 
     collection = relationship("Collection", back_populates="images")
+    locations = relationship("ImageLocation", back_populates="image")
 
     def __repr__(self):
         return "<Image(name='%s', note='%s', url='%s', collection='%s')>" % \
             (self.image_id, self.image_note, self.image_url, self.collection)
+
+
+class Location(Constructor, Base):
+    __tablename__ = "location"
+
+    location_id = Column(Integer, primary_key=True, autoincrement="auto")
+    location_name = Column(String(255))
+    location_division = Column(String(255))
+    building_name = Column(String(255))
+    street_number = Column(String(30))
+    street_name = Column(String(255))
+    street_type = Column(String(30))
+    suburb = Column(String(255))
+    state = Column(String(30))
+    post_code = Column(String(30))
+    latitude = Column(Numeric)
+    longitude = Column(Numeric)
+    confidence = Column(Numeric)
+    location_type = Column(String(30))
+
+    images = relationship("ImageLocation", back_populates="location")
+    collections = relationship("CollectionLocation", back_populates="location")
+
+    def __repr__(self):
+        return "<Location(name='%s', type='%s')>" % \
+            (self.location_id, self.location_type)
+
+
+##########################################################
+# Relationship Tables
 
 
 class CollectionSubject(Constructor, Base):
@@ -123,13 +156,21 @@ class CollectionTopic(Constructor, Base):
 class CollectionLocation(Constructor, Base):
     __tablename__ = "collection_location"
 
-    collection_id = Column(
-        String(30),
-        ForeignKey("collection.collection_id"),
-        primary_key=True)
-    location = Column(String(255), primary_key=True)
+    collection_id = Column(String(30), ForeignKey("collection.collection_id"), primary_key=True)
+    location_id = Column(Integer, ForeignKey("location.location_id"), primary_key=True)
 
     collection = relationship("Collection", back_populates="locations")
+    location = relationship("Location", back_populates="collections")
+
+
+class ImageLocation(Constructor, Base):
+    __tablename__ = "image_location"
+
+    image_id = Column(String(30), ForeignKey("image.image_id"), primary_key=True)
+    location_id = Column(Integer, ForeignKey("location.location_id"), primary_key=True)
+
+    image = relationship("Image", back_populates="locations")
+    location = relationship("Location", back_populates="images")
 
 
 ##########################################################

a6b476e4393c5a2c1ba563e265be2420d3287277
Mark Shelton <mark@shelton.id.au>
2018-01-29 19:42:58 +0800

    improved and simplified cli, integrated with logging

--- thickshake/cli.py
+++ thickshake/cli.py
@@ -6,6 +6,8 @@
 # Standard Library Imports
 
 import configparser
+import functools
+import logging
 import os
 
 ##########################################################
@@ -13,19 +15,20 @@ import os
 
 import click
 from click_help_colors import HelpColorsGroup, HelpColorsCommand
+import click_log
+from envparse import env
 
 ##########################################################
 # Local Imports
 
-from thickshake import main
-from thickshake.datastore import Store
-from thickshake.metadata import metadata
+from thickshake.marc import marc
 from thickshake.parser import parser
 from thickshake.image import image
 
 ##########################################################
 # Typing Configuration
 
+from typing import Any
 FilePath = str 
 DirPath = str
 
@@ -33,7 +36,7 @@ DirPath = str
 # Constants
 
 CURRENT_FILE_DIR, _ = os.path.split(__file__)
-CONFIG_FILE = "%s/settings.ini" % (CURRENT_FILE_DIR)
+CONFIG_SETTINGS_FILE = env.str("CONFIG_SETTINGS_FILE", default="%s/settings.ini" % (CURRENT_FILE_DIR))
 
 ##########################################################
 # Classes
@@ -54,13 +57,24 @@ class MyParser(configparser.ConfigParser):
 # Initializations
 
 parser = MyParser()
-parser.read(CONFIG_FILE)
+parser.read(CONFIG_SETTINGS_FILE)
 default_map = parser.as_dict()
 context_settings = dict(default_map=default_map, ignore_unknown_options=True, allow_extra_args=True)
-store = Store()
+logger = logging.getLogger()
 
 ##########################################################
-# Functions
+# Helpers
+
+def common_params(func):
+    @click.option("-f", "--force", is_flag=True, help="overwrite existing files")
+    @click.option("-n", "--dry-run", is_flag=True, help="run without creating files")
+    @click.option("-d", "--display", is_flag=True, help="display images in GUI")
+    @click.option("-s", "--sample", type=int, default=0, help="perform on random sample (default: 0 / None)")
+    @click_log.simple_verbosity_option(logger)
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+    return wrapper
 
 
 @click.group(
@@ -69,63 +83,67 @@ store = Store()
     help_options_color='green',
     context_settings=context_settings,
 )
-@click.option("-v/-q", "--verbose/--quiet", default=False, help="logging")
-@click.option("-f", "--force", is_flag=True, help="overwrite existing files")
-@click.option("-n", "--dry-run", is_flag=True, help="run without creating files")
-@click.option("-d", "--display", is_flag=True, help="display images in GUI")
-@click.option("-s", "--sample", type=int, default=20, help="perform on random sample")
-@click.pass_context
-def cli(ctx, **kwargs):
+@common_params
+def cli(**kwargs):
     """Functions for improving library catalogues."""
-    ctx.obj = kwargs
+    pass
+
+
+##########################################################
+# Functions
 
 
 @cli.command(context_settings=context_settings)
-@click.argument("input_metadata_file", type=click.Path(exists=True, dir_okay=False))
-@click.argument("output_metadata_file", required=False, type=click.Path(exists=True, dir_okay=False))
-@click.argument("input_image_dir", required=False, type=click.Path(exists=True, file_okay=False))
-@click.option("--diff", is_flag=True, help="generate change log from metadata files")
-@click.pass_context
-def augment(ctx, input_metadata_file: FilePath, output_metadata_file: FilePath, input_image_dir: DirPath, diff: bool) -> None:
-    """Applies techniques to improve the metadata."""
-    parser.augment_metadata(input_metadata_file, output_metadata_file, input_image_dir, diff, **ctx.obj)
+@click.option("-i", "--input-metadata-file", required=True, type=click.Path(exists=True, dir_okay=False))
+@common_params
+def load(input_metadata_file: FilePath, **kwargs: Any) -> None:
+    """Imports metadata into database."""
+    marc.import_metadata(input_metadata_file, **kwargs)
 
 
 @cli.command(context_settings=context_settings)
-@click.argument("input_metadata_file", type=click.Path(exists=True, dir_okay=False))
-@click.argument("output_metadata_file", type=click.Path(exists=True, dir_okay=False))
-@click.pass_context
-def convert(ctx, input_metadata_file: FilePath, output_metadata_file: FilePath) -> None:
+@click.option("-o", "--output-metadata-file", required=True, type=click.Path(exists=True, dir_okay=False))
+@common_params
+def load(input_metadata_file: FilePath, **kwargs: Any) -> None:
+    """Imports metadata into database."""
+    marc.export_metadata(output_metadata_file, **kwargs)
+
+
+@cli.command(context_settings=context_settings)
+@click.option("-i","--input-metadata-file", required=True, type=click.Path(exists=True, dir_okay=False))
+@click.option("-o","--output-metadata-file", required=True, type=click.Path(exists=True, dir_okay=False))
+@common_params
+def convert(input_metadata_file: FilePath, output_metadata_file: FilePath, **kwargs: Any) -> None:
     """Converts metadata between file formats."""
-    marc.convert_metadata(input_metadata_file, output_metadata_file, **ctx.obj)
+    marc.convert_metadata(input_metadata_file, output_metadata_file, **kwargs)
 
 
 @cli.command(context_settings=context_settings)
-@click.argument("input_image_dir", type=click.Path(exists=True, file_okay=False))
-@click.argument("output_image_dir", required=False, type=click.Path(exists=True, file_okay=False))
+@click.option("-i", "--input-image-dir", required=True, type=click.Path(exists=True, file_okay=False))
+@click.option("-o", "--output-image-dir", required=False, type=click.Path(exists=True, file_okay=False))
 @click.option("--func", type=click.Choice(["face", "caption", "text", "all"]), default="all", multiple=True, help="Image processing function")
-@click.pass_context
-def process(ctx, func: str, input_image_dir: DirPath, output_image_dir: DirPath) -> None:
+@common_params
+def process(func: str, input_image_dir: DirPath, output_image_dir: DirPath, **kwargs: Any) -> None:
     """Performs processing on the images."""
     input_images = [cv2.imread(image_file) for image_file in input_image_dir]
     if "all" in func: func = ["face", "caption", "text"]
-    if "face" in func: image.extract_faces(input_images, output_image_dir, **ctx.obj)
-    if "text" in func: image.read_text(input_images, output_image_dir, **ctx.obj)
-    if "caption" in func: image.caption_image(input_images, output_image_dir, **ctx.obj)
+    if "face" in func: image.extract_faces(input_images, output_image_dir, **kwargs)
+    if "text" in func: image.read_text(input_images, output_image_dir, **kwargs)
+    if "caption" in func: image.caption_image(input_images, output_image_dir, **kwargs)
 
 
 @cli.command(context_settings=context_settings)
-@click.argument("input_metadata_file", required=False, type=click.Path(exists=True, dir_okay=False))
-@click.argument("output_metadata_file", required=False, type=click.Path(exists=True, dir_okay=False))
+@click.option("-i", "--input-metadata-file", required=False, type=click.Path(exists=True, dir_okay=False))
+@click.option("-o", "--output-metadata-file", required=False, type=click.Path(exists=True, dir_okay=False))
 @click.option("--func", type=click.Choice(["date", "location", "link", "all"]), default="all", multiple=True, help="Metadata parsing function")
 @click.pass_context
-def parse(ctx, func: str, input_metadata_file: DirPath, output_metadata_file: DirPath) -> None:
+def parse(func: str, input_metadata_file: DirPath, output_metadata_file: DirPath, **kwargs: Any) -> None:
     """Performs parsing on the metadata."""
     if "all" in func: func = ["date", "location", "link"]
-    if "date" in func: parser.parse_dates(input_metadata_file, **ctx.obj)
-    if "location" in func: parser.parse_locations(input_metadata_file, **ctx.obj)
-    if "link" in func: parser.parse_links(input_metadata_file, **ctx.obj)
-    metadata.export_database(output_metadata_file, **ctx.obj)
+    if "date" in func: parser.parse_dates(input_metadata_file, **kwargs)
+    if "location" in func: parser.parse_locations(input_metadata_file, **kwargs)
+    if "link" in func: parser.parse_links(input_metadata_file, **kwargs)
+    metadata.export_database(output_metadata_file, **kwargs)
 
 
 ##########################################################
